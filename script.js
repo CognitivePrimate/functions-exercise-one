93bf01b9ad87d876e4eb55ca8d07c0bafb872c17
Derek Jackson <d4jackson84@gmail.com>
2021-04-26 19:05:53 -0400

    fixed for my inability to read directions

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 
 function getAreaOfCirle(r) {
     if (r >= 0) {
-        console.log(Math.PI * (r * r));
+       return Math.PI * (r * r);
     }
 }
 
-getAreaOfCirle(5);
-
 
 function getCircumferenceOfCircle(r) {
-    console.log(2 * Math.PI * r);
+    return 2 * Math.PI * r;
 }
 
-getCircumferenceOfCircle(5);
+
 
 
 function getAreaOfSquare(side) {
-    console.log(side * side);
+     return side * side
 }
 
-getAreaOfSquare(5);
+
 
 function getAreaOfTriangle(base, height) {
-    console.log((base * height) /2);
+    return (base * height) /2;
 }
 
-getAreaOfTriangle(4, 8);
 
+
+console.log(getAreaOfCirle(5));
+console.log(getCircumferenceOfCircle(5));
+console.log(getAreaOfSquare(5));
+console.log(getAreaOfTriangle(4, 8));
